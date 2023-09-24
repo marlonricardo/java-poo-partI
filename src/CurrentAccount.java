1083d5587321d7355dd8785d2d455081b766d592
marlonricardo <mroparaty@gmail.com>
2023-09-24 14:46:28 -0300

    Added TODO

--- src/CurrentAccount.java
+++ src/CurrentAccount.java
@@ -1,6 +1,7 @@
 public class CurrentAccount extends Account{
     private final double monthTax = 15;
 
+    //TODO check if it is possible making validations inside constructors.
     public CurrentAccount(int agency, int number, String owner, double balance){
         super(agency, number, owner, balance);
         if(balance - monthTax < 0){
